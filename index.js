628a809ed3c5ca080d149cc947423bd01c456fa9
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-19 23:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  name.push()
-  return array
+  kitten.push(name)
+  return kitten
 }
